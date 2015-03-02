e53b0b66f936678ac02d754c6eba4ed89910147b
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-03-01 19:18:06 -0700

    error catching

--- dbcontroller.js
+++ dbcontroller.js
@@ -85,6 +85,9 @@ app.post('/newUser', function (req, res) {
                     connection.query("INSERT INTO Users (email, password, name, age) VALUES (\"" + req.body.email + "\", \"" + req.body.password + "\", \"" + req.body.name + "\", " + req.body.age + ")", function (err, rows) {
                         console.log("Inserting into users.");
                         console.log("error: ", err);
+                        if(err){
+                            return err;
+                        }
                         // Adds the user info to the Users table
                         // TODO return the token information back to the POST response
                         res.status(200).send("User successfully created").end(); // Send a confirmation that the user was created and a code 200
