bd996e5e60b1a5d543d7ca963723e3215cb9fc9a
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-03-01 18:16:52 -0700

    trying to create token

--- dbcontroller.js
+++ dbcontroller.js
@@ -25,12 +25,16 @@ app.use(bodyParser.json()); // Add support for JSON-encoded bodies
 app.use(bodyParser.urlencoded({
     extended: true
 })); // Add support for URL-encoded bodies
-app.use((expressJwt({secret: jwtSecret }).unless({path: ['/login']})));
+app.use((expressJwt({
+    secret: jwtSecret
+}).unless({
+    path: ['/login']
+})));
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
@@ -43,7 +47,7 @@ app.post('/nearby', function (req, res) {
         console.log(req.body.latitude); // Debugging
         console.log(req.body.longitude); // Debugging
         pool.getConnection(function (err, connection) { // Create the connection to the databasae, passed as connection to the function
-            connection.query(getZoneQuery([req.body.latitude,req.body.longitude]), function (err, rows) { // Send the query asking for the full table of that zip code and the 00000 zip code to the db, returns the rows json object, passed to the function
+            connection.query(getZoneQuery([req.body.latitude, req.body.longitude]), function (err, rows) { // Send the query asking for the full table of that zip code and the 00000 zip code to the db, returns the rows json object, passed to the function
                 console.log(rows); // Debugging
                 if (!err) {
                     // If there is no error from the db
@@ -70,25 +74,28 @@ app.post('/nearby', function (req, res) {
 });
 
 app.post('/newUser', function (req, res) {
-        // If the POST object is an attempt to make a new user
-        pool.getConnection(function (err, connection) {
-            // Make a connection the the db
-            connection.query("SELECT EXISTS(SELECT 1 FROM Users WHERE email = \"" + req.body.email + "\")", function (err, rows) {
-                // Return a query "rows" that contains a 1 if the email exists, 0 if not
-                console.log(rows); // Debugging
-                if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
-                    res.status(403).send("Email already exists").end(); //  If 1, send a response saying the email is already in the server
-                } else {
-                    // TODO login stuff here, creating a token to send back and one to store
-                    connection.query("INSERT INTO Users (email, password, name, age) VALUES (\"" + req.body.email + "\", \"" + req.body.password + "\", \"" + req.body.name + "\", " + req.body.age + ")", function (err, rows) {
-                        // Adds the user info to the Users table
-                        // TODO return the token information back to the POST response
-                        res.status(200).send("User successfully created").end(); // Send a confirmation that the user was created and a code 200
-                    })
-                }
-                connection.release(); // Release db connection to pool
-            });
+    // If the POST object is an attempt to make a new user
+    pool.getConnection(function (err, connection) {
+        // Make a connection the the db
+        connection.query("SELECT EXISTS(SELECT 1 FROM Users WHERE email = \"" + req.body.email + "\")", function (err, rows) {
+            // Return a query "rows" that contains a 1 if the email exists, 0 if not
+            console.log(rows); // Debugging
+            if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
+                res.status(403).send("Email already exists").end(); //  If 1, send a response saying the email is already in the server
+            } else {
+                var token = jwt.sign({
+                    user: req.body.email
+                }, jwtSecret);
+                // TODO login stuff here, creating a token to send back and one to store
+                connection.query("INSERT INTO Users (email, password, name, age) VALUES (\"" + req.body.email + "\", \"" + req.body.password + "\", \"" + req.body.name + "\", " + req.body.age + ")", function (err, rows) {
+                    // Adds the user info to the Users table
+                    // TODO return the token information back to the POST response
+                    res.status(200).send("User successfully created").end(); // Send a confirmation that the user was created and a code 200
+                })
+            }
+            connection.release(); // Release db connection to pool
         });
+    });
 });
 
 app.post('/login', authenticate, function (req, res) {
@@ -101,13 +108,13 @@ app.post('/login', authenticate, function (req, res) {
     });
 });
 
-app.get('/random-user', function(req, res) {
+app.get('/random-user', function (req, res) {
     var user = faker.helpers.userCard();
     user.avatar = faker.image.avatar();
     res.json(user);
 });
 
-app.get('/me', function(req, res) {
+app.get('/me', function (req, res) {
     console.log(req.user);
     res.send(req.user);
 });
@@ -131,16 +138,16 @@ function authenticate(req, res, next) {
             // Make a connection the the db
             connection.query("SELECT EXISTS(SELECT 1 FROM Users WHERE email = \"" + req.body.email + "\")", function (err, rows) {
                 // Return a query "rows" that contains a 1 if the email exists, 0 if not
-                console.log("rows: ",rows[0], "Object Keys: ", rows[0][Object.keys(rows[0])[0]]); // Debugging
+                console.log("rows: ", rows[0], "Object Keys: ", rows[0][Object.keys(rows[0])[0]]); // Debugging
                 if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
                     console.log("Email exists");
                     connection.query("SELECT password FROM Users WHERE email = \"" + req.body.email + "\"", function (err, psswd) {
-                    	if (psswd[0] == req.body.password) {
-                    		// Do stuff here if password is right
+                        if (psswd[0] == req.body.password) {
+                            // Do stuff here if password is right
                             console.log("password is correct", req.body.password);
-                    	} else {
-                    		res.status(401).end('Incorrect password')
-                    	};
+                        } else {
+                            res.status(401).end('Incorrect password')
+                        };
                     })
                 } else {
                     console.log("User doesn't exist.");
@@ -153,96 +160,94 @@ function authenticate(req, res, next) {
     next();
 };
 
-function zoneLookup (arr) {
-	var solution = "zone_";
-	var lat = "";
-	var lon = "";
+function zoneLookup(arr) {
+    var solution = "zone_";
+    var lat = "";
+    var lon = "";
 
-	lat = Math.round(arr[0]) + "";
-	if (arr[0] < 0) {
-		solution = solution + "n";
-	};
-	solution = solution + lat;
-	lon = Math.round(arr[1]) + "";
-	if (arr[1] < 0) {
-		solution = solution + "n";
-	};
-	solution = solution + lon;
+    lat = Math.round(arr[0]) + "";
+    if (arr[0] < 0) {
+        solution = solution + "n";
+    };
+    solution = solution + lat;
+    lon = Math.round(arr[1]) + "";
+    if (arr[1] < 0) {
+        solution = solution + "n";
+    };
+    solution = solution + lon;
 
-	return solution;
+    return solution;
 }
 
 function getZoneQuery(arr) {
-	var lat = "";
-	var lon = "";
-	var solution = "SELECT * FROM worldwide UNION ALL SELECT * FROM zone_";
-	var additionalzones = "";
+    var lat = "";
+    var lon = "";
+    var solution = "SELECT * FROM worldwide UNION ALL SELECT * FROM zone_";
+    var additionalzones = "";
 
-	var lonpositive = false;
-	var latpositive = false;
-	var closetolat = false;
-	var latclosetof = false;
-	var closetolon = false;
-	var lonclosetof = false;
+    var lonpositive = false;
+    var latpositive = false;
+    var closetolat = false;
+    var latclosetof = false;
+    var closetolon = false;
+    var lonclosetof = false;
 
-	if (arr[0] >= 0) {
-		latpositive = true;
-		lat = Math.floor(arr[0]).toString();
-		console.log("Lat is positive");
-	} else {
-		lat = "n" + Math.abs(Math.ceil(arr[0]));
-		console.log("Lat is negative");
-	};
-	if (arr[1] >= 0) {
-		lonpositive = true;
-		lon = Math.floor(arr[1]).toString();
-		console.log("Lon is positive");
-	} else {
-		lon = "n" + Math.abs(Math.ceil(arr[1]));
-		console.log("Lon is negative");
-	};
+    if (arr[0] >= 0) {
+        latpositive = true;
+        lat = Math.floor(arr[0]).toString();
+        console.log("Lat is positive");
+    } else {
+        lat = "n" + Math.abs(Math.ceil(arr[0]));
+        console.log("Lat is negative");
+    };
+    if (arr[1] >= 0) {
+        lonpositive = true;
+        lon = Math.floor(arr[1]).toString();
+        console.log("Lon is positive");
+    } else {
+        lon = "n" + Math.abs(Math.ceil(arr[1]));
+        console.log("Lon is negative");
+    };
 
-	if (latpositive) {
-		if (Math.abs(Math.round(arr[0]) - arr[0]) < 0.1) {
-			closetolat = true;
-			if (Math.round(arr[0]) == Math.floor(arr[0])) {
-				latclosetof = true;
-			};
-			additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + Math.round(arr[0]) + "_" + lon;
-		};
-	} else {
-		if (Math.abs(Math.round(arr[0]) - arr[0]) < 0.1) {
-			closetolat = true;
-			if (Math.round(arr[0]) == Math.ceil(arr[0])) {
-			};
-			additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_n" + Math.abs(Math.round(arr[0])) + "_" + lon;
-		};
-	};
+    if (latpositive) {
+        if (Math.abs(Math.round(arr[0]) - arr[0]) < 0.1) {
+            closetolat = true;
+            if (Math.round(arr[0]) == Math.floor(arr[0])) {
+                latclosetof = true;
+            };
+            additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + Math.round(arr[0]) + "_" + lon;
+        };
+    } else {
+        if (Math.abs(Math.round(arr[0]) - arr[0]) < 0.1) {
+            closetolat = true;
+            if (Math.round(arr[0]) == Math.ceil(arr[0])) {};
+            additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_n" + Math.abs(Math.round(arr[0])) + "_" + lon;
+        };
+    };
 
-	if (lonpositive) {
-		if (Math.abs(Math.round(arr[1]) - arr[1]) < 0.1) {
-			closetolat = true;
-			if (Math.round(arr[1]) == Math.floor(arr[1])) {
-				lonclosetof = true;
-			};
-			additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + lat + "_" + Math.round(arr[1]);
-		};
-	} else {
-		if (Math.abs(Math.round(arr[1]) - arr[1]) < 0.1) {
-			closetolat = true;
-			if (Math.round(arr[1]) == Math.ceil(arr[1])) {
-			};
-			additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + lat + "_n" + Math.abs(Math.round(arr[1]));
-		};
-	};
+    if (lonpositive) {
+        if (Math.abs(Math.round(arr[1]) - arr[1]) < 0.1) {
+            closetolat = true;
+            if (Math.round(arr[1]) == Math.floor(arr[1])) {
+                lonclosetof = true;
+            };
+            additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + lat + "_" + Math.round(arr[1]);
+        };
+    } else {
+        if (Math.abs(Math.round(arr[1]) - arr[1]) < 0.1) {
+            closetolat = true;
+            if (Math.round(arr[1]) == Math.ceil(arr[1])) {};
+            additionalzones = additionalzones + " UNION ALL SELECT * FROM zone_" + lat + "_n" + Math.abs(Math.round(arr[1]));
+        };
+    };
 
-	if (closetolat && closetolon) {
-		// TODO add code to add the corner zones to the query
-	};
+    if (closetolat && closetolon) {
+        // TODO add code to add the corner zones to the query
+    };
 
-	solution = solution + lat + "_" + lon;
-	console.log("Original solution: " + solution);
-	solution = solution + additionalzones;
-	console.log("Changed solution: " + solution);
-	return solution;
+    solution = solution + lat + "_" + lon;
+    console.log("Original solution: " + solution);
+    solution = solution + additionalzones;
+    console.log("Changed solution: " + solution);
+    return solution;
 }
