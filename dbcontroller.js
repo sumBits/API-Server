81e2c2722de1d9053dd438d151a2e0708d8fc46b
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-03-01 18:55:48 -0700

    debug messages added

--- dbcontroller.js
+++ dbcontroller.js
@@ -77,11 +77,13 @@ app.post('/newUser', function (req, res) {
                 // Return a query "rows" that contains a 1 if the email exists, 0 if not
                 console.log(rows); // Debugging
                 if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
+                    console.log("Email exists already.");
                     res.status(403).send("Email already exists").end(); //  If 1, send a response saying the email is already in the server
                 } else {
-
+                    console.log("Reached else in new user, meaning new user will be created.");
                     // TODO login stuff here, creating a token to send back and one to store
                     connection.query("INSERT INTO Users (email, password, name, age) VALUES (\"" + req.body.email + "\", \"" + req.body.password + "\", \"" + req.body.name + "\", " + req.body.age + ")", function (err, rows) {
+                        console.log("Inserting into users.");
                         // Adds the user info to the Users table
                         // TODO return the token information back to the POST response
                         res.status(200).send("User successfully created").end(); // Send a confirmation that the user was created and a code 200
