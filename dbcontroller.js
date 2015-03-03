4426a9b73696cdab2a90e8271b8dadcc0cd0f647
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-03-02 17:58:09 -0700

    only one end statement

--- dbcontroller.js
+++ dbcontroller.js
@@ -133,7 +133,7 @@ function authenticate(req, res, next) {
     console.log("Auth Called");
     var body = req.body;
     if (!body.email || !body.password) {
-        res.status(400).end('Must provide email or password')
+        res.status(400).write('Must provide email or password')
     } else {
         pool.getConnection(function (err, connection) {
             // Make a connection the the db
@@ -151,15 +151,14 @@ function authenticate(req, res, next) {
                     	} else {
                             console.log('password is incorrect');
                     		res.status(401).write('Incorrect password');
-                            res.end();
                     	};
                     })
                 } else {
                     console.log("User doesn't exist.");
                     res.status(401).write('Incorrect email or password');
-                    res.end();
                 }
                 connection.release(); // Release db connection to pool
+                res.end();
             });
         });
     };
