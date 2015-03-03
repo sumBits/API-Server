460ab7a313daaf57d03993b2175ea352658defe9
Spencer P <stpoz@live.com>
2015-03-03 10:57:35 -0700

    More modules stuff
    Moved users functions to modules, we will see if this works
    (dependencies are a pain)

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var expressJwt = require('express-jwt');
 var jwtSecret = 'fjkdlsajfoew239053/3uk';
 
 // Modules
-var nearby = require('./nearby')
+var nearby = require('./nearby');
+var users = require('./users');
 
 // Set up mysql pool for creating connections to db
 var pool = mysql.createPool({
@@ -38,6 +39,7 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Read-only post checking, while user isn't logged in
 app.post('/nearbyRO', function(req, res) {
     console.log("Recieved post requesting Read-Only posts");
     if (req.get('Object-Type') == 'location') {
@@ -57,72 +59,11 @@ app.post('/nearby', function (req, res) {
 });
 
 app.post('/newUser', function (req, res) {
-    // If the POST object is an attempt to make a new user
-    pool.getConnection(function (err, connection) {
-        // Make a connection the the db
-        connection.query("SELECT EXISTS(SELECT 1 FROM Users WHERE email = \"" + req.body.email + "\")", function (err, rows) {
-            // Return a query "rows" that contains a 1 if the email exists, 0 if not
-            console.log(rows); // Debugging
-            if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
-                console.log("Email exists already.");
-                res.status(403).send("Email already exists").end(); //  If 1, send a response saying the email is already in the server
-            } else {
-                console.log("Reached else in new user, meaning new user will be created.");
-                // TODO login stuff here, creating a token to send back and one to store
-                var q = connection.query("INSERT INTO Users SET ?", req.body, function (err, rows) {
-                    console.log("Inserting into users.");
-                    console.log("error: ", err);
-                    if (err) {
-                        res.status(500).send("The user failed to create.").end();
-                    }
-                    // Adds the user info to the Users table
-                    // TODO return the token information back to the POST response
-                    res.status(200).send("User successfully created").end(); // Send a confirmation that the user was created and a code 200
-                });
-                console.log(q.sql);
-            }
-            connection.release(); // Release db connection to pool
-        });
-    });
+    users.newUser(req, res, pool);
 });
 
 app.post('/login', function (req, res) {
-    var token = jwt.sign({
-        user: req.body.email
-    }, jwtSecret);
-    var body = req.body;
-    if (!body.email || !body.password) {
-        res.status(400).end('Must provide email or password')
-    } else {
-        pool.getConnection(function (err, connection) {
-            // Make a connection the the db
-            connection.query("SELECT EXISTS(SELECT 1 FROM Users WHERE email = \"" + req.body.email + "\")", function (err, rows) {
-                // Return a query "rows" that contains a 1 if the email exists, 0 if not
-                console.log("rows: ", rows[0], "Object Keys: ", rows[0][Object.keys(rows[0])[0]]); // Debugging
-                if (rows[0][Object.keys(rows[0])[0]] == 1) { // Access the 0 or 1
-                    console.log("Email exists");
-                    connection.query("SELECT password FROM Users WHERE email = \"" + req.body.email + "\"", function (err, psswd) {
-                        console.log(psswd[0]);
-                        console.log(psswd[0].password);
-                        if (psswd[0].password == req.body.password) {
-                            res.send({
-                                token: token,
-                                user: req.body.email
-                            }).end();
-                            console.log("password is correct", req.body.password);
-                        } else {
-                            console.log('password is incorrect');
-                            res.status(401).end('Incorrect password');
-                        };
-                    })
-                } else {
-                    console.log("User doesn't exist.");
-                    res.status(401).end('Incorrect email or password');
-                }
-                connection.release(); // Release db connection to pool
-            });
-        });
-    };
+    users.login(req, res, pool);
 });
 
 app.get('/random-user', function (req, res) {
@@ -153,23 +94,4 @@ function authenticate(req, res, next) {
         }
     });
     next();
-};
-
-function zoneLookup(arr) {
-    var solution = "zone_";
-    var lat = "";
-    var lon = "";
-
-    lat = Math.round(arr[0]) + "";
-    if (arr[0] < 0) {
-        solution = solution + "n";
-    };
-    solution = solution + lat;
-    lon = Math.round(arr[1]) + "";
-    if (arr[1] < 0) {
-        solution = solution + "n";
-    };
-    solution = solution + lon;
-
-    return solution;
 };
