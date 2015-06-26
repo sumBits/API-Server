99424f07bb879804caeab4c44ec557f05983d5f4
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-06-26 10:55:37 -0600

    bug fix

--- controllers/userThreads.js
+++ controllers/userThreads.js
@@ -13,6 +13,6 @@ exports.getUserThreads = function(req, res, pool){
 
 exports.postToThread = function(req, res, pool){
     console.log(req.body);
-    console.log("Posting to thread " + req.body.threadId + " the following content: " + req.body.content);
+    console.log("Posting to thread " + req.body.data.threadId + " the following content: " + req.body.data.content);
     res.send("nothing").end();
 };
