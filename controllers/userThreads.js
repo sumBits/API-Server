6074ddfe7e2508e3fde2e39346242b209beb08d5
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-06-26 10:40:24 -0600

    ending connections

--- controllers/userThreads.js
+++ controllers/userThreads.js
@@ -2,16 +2,16 @@
 
 exports.getPostsInThread = function(req, res, pool){
     console.log("Getting posts in thread: " + req.body.threadId);
-    res.send(null);
+    res.send(null).end();
 };
 
 exports.getUserThreads = function(req, res, pool){
     console.log(req.body);
     console.log("Getting user threads for user: " + req.body.data);
-    res.send(null);
+    res.send(null).end();
 };
 
 exports.postToThread = function(req, res, pool){
     console.log("Posting to thread " + req.body.threadId + " the following content: " + req.body.post.content);
-    res.send(null);
+    res.send(null).end();
 };
