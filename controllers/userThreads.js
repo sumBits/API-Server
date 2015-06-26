079bada6652abc59e174b62111ca5896e9249675
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-06-26 10:43:09 -0600

    debug

--- controllers/userThreads.js
+++ controllers/userThreads.js
@@ -2,16 +2,16 @@
 
 exports.getPostsInThread = function(req, res, pool){
     console.log("Getting posts in thread: " + req.body.threadId);
-    res.send(0).end();
+    res.send("nothing").end();
 };
 
 exports.getUserThreads = function(req, res, pool){
     console.log(req.body);
     console.log("Getting user threads for user: " + req.body.data);
-    res.send(0).end();
+    res.send("nothing").end();
 };
 
 exports.postToThread = function(req, res, pool){
     console.log("Posting to thread " + req.body.threadId + " the following content: " + req.body.post.content);
-    res.send(0).end();
+    res.send("nothing").end();
 };
