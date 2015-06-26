bd977bad56f3a9fce6b273748f3c34f185dfefc3
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-06-26 10:32:04 -0600

    trying to fix things

--- controllers/userThreads.js
+++ controllers/userThreads.js
@@ -2,12 +2,15 @@
 
 exports.getPostsInThread = function(req, res, pool){
     console.log("Getting posts in thread: " + req.body.threadId);
+    res.send(null);
 };
 
 exports.getUserThreads = function(req, res, pool){
     console.log("Getting user threads for user: " + req.body.user);
+    res.send(null);
 };
 
 exports.postToThread = function(req, res, pool){
     console.log("Posting to thread " + req.body.threadId + " the following content: " + req.body.post.content);
+    res.send(null);
 };
