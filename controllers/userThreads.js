3af3575d84be251a8b95ae02548b50c98176c139
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-06-26 13:17:14 -0600

    missing brackets fix

--- controllers/userThreads.js
+++ controllers/userThreads.js
@@ -1,51 +1,51 @@
 'use strict';
 
-exports.getPostsInThread = function(req, res, pool){
+exports.getPostsInThread = function (req, res, pool) {
     console.log("Getting posts in thread: " + req.body.data);
     var threadId = req.body.data;
-     pool.getConnection(function (err, connection) { // Create the connection to the databasae, passed as connection to the function
-        connection.query("CALL getUThreadPosts(" + threadId  + ");", function(err, rows){
+    pool.getConnection(function (err, connection) { // Create the connection to the databasae, passed as connection to the function
+        connection.query("CALL getUThreadPosts(" + threadId + ");", function (err, rows) {
             console.log(rows[0]); // Debugging
             if (!err) {
                 // If there is no error from the db
                 res.status(200); // Send a 200 code (meaning there was no error)
                 res.send(rows[0]); //  Send the json object representing the rows that were received
-            } else 
+            } else {
                 res.status(500);
                 console.log(err);
             }
-            connection.release(); // Put the db connection back in the pool
-            res.end(); // Send the END packet thing to the request, ending the connection created by the POST from the app
-
         });
+        connection.release(); // Put the db connection back in the pool
+        res.end(); // Send the END packet thing to the request, ending the connection created by the POST from the app
+
     });
-    
 };
 
-exports.getUserThreads = function(req, res, pool){
+exports.getUserThreads = function (req, res, pool) {
     console.log(req.body);
     console.log("Getting user threads for user: " + req.body.data);
     var user = req.body.data;
-    
+
     pool.getConnection(function (err, connection) { // Create the connection to the databasae, passed as connection to the function
-        connection.query("CALL getUserThreads(" + user + ");", function(err, rows){
+        connection.query("CALL getUserThreads(" + user + ");", function (err, rows) {
             console.log(rows[0]); // Debugging
             if (!err) {
                 // If there is no error from the db
                 res.status(200); // Send a 200 code (meaning there was no error)
                 res.send(rows[0]); //  Send the json object representing the rows that were received
-            } else 
+            } else {
                 res.status(500);
                 console.log(err);
             }
-            connection.release(); // Put the db connection back in the pool
-            res.end(); // Send the END packet thing to the request, ending the connection created by the POST from the app
-
         });
+        connection.release(); // Put the db connection back in the pool
+        res.end(); // Send the END packet thing to the request, ending the connection created by the POST from the app
+
     });
+    
 };
 
-exports.postToThread = function(req, res, pool){
+exports.postToThread = function (req, res, pool) {
     console.log(req.body);
     console.log("Posting to thread " + req.body.data.threadId + " the following content: " + req.body.data.content);
     res.send("nothing").end();
